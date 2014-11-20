2f4f3e765a80cb81b1bfaf701521761404aa7bb2
Jackson Owens <jackson@squareup.com>
2014-11-20 14:22:07 -0800

    Fiddle with travis-ci dependencies script

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -2,17 +2,11 @@
 
 sudo apt-get install git
 sudo apt-get install build-essential
-cd /usr/local/lib/
 git clone git@github.com:sass/sassc.git
 git clone git@github.com:sass/libsass.git
 # Initialize and update the submodule sass2scss…
 cd libsass/
 git submodule update –-init
-export SASS_LIBSASS_PATH="/usr/local/lib/libsass"
+export SASS_LIBSASS_PATH="`pwd`/libsass"
 # Make sure it worked…
 echo $SASS_LIBSASS_PATH
-# Now you can make SassC…
-cd /usr/local/lib/sassc/
-make
-cd /usr/local/bin/
-ln -s ../lib/sassc/bin/sassc sassc
