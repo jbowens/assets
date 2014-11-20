f30331a18106c7fc2caf09c37c0f2760c569d2ec
Jackson Owens <jackson@squareup.com>
2014-11-20 15:24:40 -0800

    Made end-to-end pipeline test more robust

--- pipeline_test.go
+++ pipeline_test.go
@@ -1,10 +1,19 @@
 package assets
 
 import (
+	"io/ioutil"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func expectFile(t *testing.T, path, contents string) {
+	bytes, err := ioutil.ReadFile(path)
+	assert.Nil(t, err)
+
+	fileContents := string(bytes)
+	assert.Equal(t, contents, fileContents)
+}
 
 func TestSimplePipeline(t *testing.T) {
 	bundle, err := Dir("test_files/abc").AllFiles().
@@ -12,4 +21,5 @@ func TestSimplePipeline(t *testing.T) {
 
 	assert.Nil(t, err)
 	assert.NotNil(t, bundle)
+	expectFile(t, "test_files/generated/abc", "a\nb\nc\n")
 }
