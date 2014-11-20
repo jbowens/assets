62982f19999b6b678a41e180e94047755a9dcd1d
Jackson Owens <jackson@squareup.com>
2014-11-20 11:23:43 -0800

    Add concat test

---
+++ concat_test.go
@@ -0,0 +1,16 @@
+package assets
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConcatEndToEnd(t *testing.T) {
+	d := Dir("test_files/abc")
+	bundle := d.AllFiles().Filter(Concat())
+	filenames, contents := bundleToFilenamesAndContents(bundle)
+
+	assert.Contains(t, filenames, "test_files/abc")
+	assert.Contains(t, contents, "a\nb\nc")
+}
