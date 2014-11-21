0656ba77582acde6f4e91a61a1901812a8a1d17f
Jackson Owens <jackson_owens@alumni.brown.edu>
2014-11-20 23:57:12 -0800

    typescript as a dependency

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-sudo apt-get install git
-sudo apt-get install build-essential
-git clone git@github.com:sass/sassc.git
-git clone git@github.com:sass/libsass.git
-# Initialize and update the submodule sass2scss…
-cd libsass/
-git submodule update –-init
-export SASS_LIBSASS_PATH="`pwd`/libsass"
-# Make sure it worked…
-echo $SASS_LIBSASS_PATH
+npm install -g typescript
