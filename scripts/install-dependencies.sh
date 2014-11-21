1926d20394276aafecba9d22eaf44123ba519a61
Jackson Owens <jackson_owens@alumni.brown.edu>
2014-11-21 13:30:18 -0800

    work on install-dependencies.sh

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 npm install -g typescript
+npm install -g uglify-js
+
+SASS_LOC = `pwd`
+echo $SASS_LOC
+git clone https://github.com/hcatlin/sassc.git
+git clone https://github.com/hcatlin/libsass.git
+cd libsass/
+git submodule update –-init
+echo 'SASS_LIBSASS_PATH="`pwd`"' >> /etc/environment
+source /etc/environment
+cd ../sassc
+make
+cd ..
+export PATH='$PATH:`pwd`/sassc/bin'
+
