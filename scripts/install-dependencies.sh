b224627941f799b8a877ac63221f597643693ae4
Jackson Owens <jackson_owens@alumni.brown.edu>
2014-11-21 14:53:02 -0800

    more dependencies setup

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -3,16 +3,21 @@
 npm install -g typescript
 npm install -g uglify-js
 
-SASS_LOC = `pwd`
+SASS_LOC=`pwd`
 echo $SASS_LOC
+cd ..
 git clone https://github.com/hcatlin/sassc.git
 git clone https://github.com/hcatlin/libsass.git
+git clone https://github.com/suapapa/go_sass.git
 cd libsass/
 git submodule update
-sudo echo 'SASS_LIBSASS_PATH="`pwd`"' >> /etc/environment
-sudo source /etc/environment
+export SASS_LIBSASS_PATH="`pwd`"
 cd ../sassc
 make
 cd ..
-export PATH='$PATH:`pwd`/sassc/bin'
+cd go_sass/
+sudo ./install_libsass.sh
+cd $SASS_LOC
 
+go get github.com/suapapa/go_sass
+go get github.com/stretchr/testify
