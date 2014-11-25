fa9fb3315034f7c54836bff2910b2eda86d833b4
Jackson Owens <jackson@squareup.com>
2014-11-24 16:52:03 -0800

    Concat uses most common extension

--- concat.go
+++ concat.go
@@ -1,6 +1,9 @@
 package assets
 
-import "io"
+import (
+	"io"
+	"path/filepath"
+)
 
 // Concat combines all provided assets into one asset. The resulting
 // AssetBundle will contain a single asset.
@@ -8,15 +11,25 @@ func Concat() Filter {
 	return FilterFunc(func(bundle AssetBundle) (AssetBundle, error) {
 		assets := bundle.Assets()
 		readClosers := make([]io.ReadCloser, len(assets))
+
+		var extensions = make(map[string]int)
 		for idx, asset := range assets {
 			readClosers[idx] = asset.Contents()
+			extensions[filepath.Ext(asset.FileName())]++
+		}
+
+		var mostCommonExt string
+		for ext, count := range extensions {
+			if count > extensions[mostCommonExt] {
+				mostCommonExt = ext
+			}
 		}
 
 		return &defaultBundle{
 			currentName: bundle.Name(),
 			assets: []Asset{
 				&asset{
-					fileName: bundle.Name(),
+					fileName: bundle.Name() + mostCommonExt,
 					contents: newMultiReadCloser(readClosers...),
 				},
 			},
