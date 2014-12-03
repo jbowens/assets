c3176e1cf7590280b70bdc33eb00832e4dedda78
Jackson Owens <jackson_owens@alumni.brown.edu>
2014-12-02 18:40:12 -0800

    Store assets as byte arrays in middleware

--- middleware.go
+++ middleware.go
@@ -1,10 +1,8 @@
 package assets
 
 import (
-	"io"
-	"mime"
+	"io/ioutil"
 	"net/http"
-	"path/filepath"
 	"strings"
 
 	"github.com/codegangsta/negroni"
@@ -17,15 +15,13 @@ import (
 // If you have large assets, do not use this middleware. In production you
 // should use nginx to deliver assets.
 func MemoryServingMiddleware(pathPrefix string, bundles ...AssetBundle) negroni.Handler {
-	mapOfAssets := make(map[string]Asset)
-	mapOfTypes := make(map[string]string)
+	mapOfAssets := make(map[string][]byte)
 
 	for _, bundle := range bundles {
 		for _, asset := range bundle.Assets() {
-			mapOfAssets[asset.FileName()] = asset
-			mimeType := mime.TypeByExtension(filepath.Ext(asset.FileName()))
-			if mimeType != "" {
-				mapOfTypes[asset.FileName()] = mimeType
+			bytes, err := ioutil.ReadAll(asset.Contents())
+			if err != nil {
+				mapOfAssets[asset.FileName()] = bytes
 			}
 		}
 	}
@@ -33,14 +29,12 @@ func MemoryServingMiddleware(pathPrefix string, bundles ...AssetBundle) negroni.
 	return &servingMiddleware{
 		pathPrefix: pathPrefix,
 		assets:     mapOfAssets,
-		types:      mapOfTypes,
 	}
 }
 
 type servingMiddleware struct {
 	pathPrefix string
-	assets     map[string]Asset
-	types      map[string]string
+	assets     map[string][]byte
 }
 
 func (m *servingMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
@@ -58,10 +52,5 @@ func (m *servingMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request, ne
 		return
 	}
 
-	mimeType, exists := m.types[fileName]
-	if exists {
-		w.Header().Set("Content-Type", mimeType)
-	}
-
-	io.Copy(w, asset.Contents())
+	w.Write(asset)
 }
