56d77c6418fc3084702c0a914e774a08d1e82851
Jackson Owens <jackson@chain.com>
2016-03-28 22:41:28 -0700

    no more sass

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -3,22 +3,5 @@
 npm install -g typescript
 npm install -g uglify-js
 
-SASS_LOC=`pwd`
-echo $SASS_LOC
-cd ..
-git clone https://github.com/hcatlin/sassc.git
-git clone https://github.com/hcatlin/libsass.git
-git clone https://github.com/suapapa/go_sass.git
-cd libsass/
-git submodule update
-export SASS_LIBSASS_PATH="`pwd`"
-cd ../sassc
-make
-cd ..
-cd go_sass/
-sudo ./install_libsass.sh
-cd $SASS_LOC
-
-go get github.com/suapapa/go_sass
 go get github.com/stretchr/testify
 go get github.com/codegangsta/negroni
